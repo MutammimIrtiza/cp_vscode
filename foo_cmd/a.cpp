2b4dbeb3240e3d9b912a670d3ea400a4bfc44429
MutammimIrtiza <166048631+MutammimIrtiza@users.noreply.github.com>
2025-11-09 17:04:32 +0600

    ...

---
+++ foo_cmd/a.cpp
@@ -0,0 +1,121 @@
+// بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ //
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Extra functionality :
+// *st.find_by_order(index) = value at index
+// st.order_of_key(value) = number of elements strictly less than value
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+#define ll long long
+#define lld long double
+#define vll vector<long long>
+#define pll pair<long long, long long>
+#define vvll vector<vll>
+#define vvvll vector<vvll>
+#define ar array
+#define F first
+#define S second
+
+#define all(v) v.begin(),v.end()
+#define range(v, i, j) v.begin()+i, v.begin()+j+1
+#define For(i, a, b) for(long long i = (a); i <= (b); ++(i))
+#define L(i, a, b) for(long long i = (a); i <= (b); ++(i))
+#define R(i, a, b) for(long long i = (a); i >= (b); --(i))
+#define sz(x) (ll)(x.size())
+#define gp " "
+#define nl "\n"
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+
+#define isSet(x, i)       (((x) >> (i)) & 1)
+#define setbit(x, i)      ((x) | (1LL << (i)))
+#define resetbit(x, i)    ((x) & (~(1LL << (i))))
+#define toggleBit(x, i)   ((x) ^ (1LL << (i)))
+#define clz(x)            ((sizeof(x) == 4) ? __builtin_clz(x) : __builtin_clzll(x))
+#define ctz(x)            ((sizeof(x) == 4) ? __builtin_ctz(x) : __builtin_ctzll(x))
+#define csb(x)            ((sizeof(x) == 4) ? __builtin_popcount(x) : __builtin_popcountll(x))
+#define msb(x)            ((sizeof(x) == 4) ? 31 - clz(x) : 63 - clz(x))
+#define lsb(x)            (ctz(x))
+
+
+
+#ifdef LOCAL
+#define deb(...) debug(#__VA_ARGS__, __LINE__, __VA_ARGS__)
+
+// Generic template 
+template<class T> 
+auto _p(const T& x) -> decltype(cout << x, void()) { cout << x; }
+
+// Specialization for strings
+inline void _p(const string& s){ cout << '"' << s << '"'; }
+
+// Specialization for pairs
+template<class T, class U> 
+void _p(const pair<T,U>& p){ cout << "{"; _p(p.first); cout << ","; _p(p.second); cout << "}"; }
+
+// Specialization for containers - this takes precedence for iterable types
+template<class C> 
+auto _p(const C& c) -> decltype(c.begin(), c.end(), void()){
+    cout << "{";
+    for(auto it = c.begin(); it != c.end(); ){ _p(*it); if(++it != c.end()) cout << ", "; }
+    cout << "}";
+}
+
+template<class... A>
+void debug(const char* s, int l, A... a){
+    cout << l << "| ";
+    string t(s); stringstream ss(t); string x; vector<string> names;
+    while(getline(ss, x, ',')) { names.push_back(x); }
+    
+    size_t idx = 0;
+    auto print_arg = [&](const auto& arg) {
+        cout << (idx ? "   " : "") << names[idx] << ": ";
+        _p(arg); idx++;
+    };
+    
+    (print_arg(a), ...);
+    cout << "\n";
+}
+#else
+#define deb(...) (void)0
+#endif
+
+
+
+mt19937_64 rnd(chrono::steady_clock::now().time_since_epoch().count());
+const int dx4[4] = {0, 0, 1, -1}, dy4[4] = {1, -1, 0, 0};
+const lld pi = 2*acos(0.0);
+const int mod = 1e9 + 7;
+const int N = 200005; ///////////////////////////////////////
+const int rt = 450;
+const ll inf = 1e15; /////////////////////////////////////////////
+
+void prep(){
+    
+}
+
+ll n, m, x, y, z, q, k, u, v, w;
+vll a(N), typ(N); 
+
+void solve(){
+    
+    // testcases ?
+
+    // cleanup ?
+
+    
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    prep();
+    // int t; cin >> t; while(t--)
+    solve();
+}
