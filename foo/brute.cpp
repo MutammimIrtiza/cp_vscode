c72abf4f93ee5d45982525fe18bd35377c68967f
MutammimIrtiza <166048631+MutammimIrtiza@users.noreply.github.com>
2026-02-04 13:15:57 +0600

    ...

--- foo/brute.cpp
+++ foo/brute.cpp
@@ -1,28 +1,163 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+
 using namespace std;
-#define ll long long
-#define isSet(x, i) ((x>>i)&1)
 
-const int MOD = 1e9+7;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define L(i, a, b) for (int i = (a); i <= (b); ++i)
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+// Robust division helpers
+ll floor_div(ll a, ll b)
+{
+    if (b < 0)
+        a = -a, b = -b;
+    if (a >= 0)
+        return a / b;
+    return (a - b + 1) / b;
+}
 
-    int t; cin >> t; 
-    while(t--) {
-        int n; string a, b;
-        cin >> n >> a >> b;
-        int ans = 0;
+ll ceil_div(ll a, ll b)
+{
+    if (b < 0)
+        a = -a, b = -b;
+    if (a >= 0)
+        return (a + b - 1) / b;
+    return a / b;
+}
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                string x = a.substr(0, i+1) + b.substr(j, n-j);
-                string y = a.substr(j, n-j) + b.substr(0, i+1);
-                ans += (x==y);
-            }
+void solve()
+{
+    int n, q;
+    if (!(cin >> n >> q))
+        return;
+    vector<ll> v(n + 1), t(n + 1);
+    L(i, 1, n)
+        cin >> t[i] >> v[i];
+
+    // Each event is {time, delta_change}
+    vector<pair<ll, int>> events;
+    auto add_range = [&](ll l, ll r)
+    {
+        if (l <= r)
+        {
+            events.push_back({l, 1});
+            events.push_back({r + 1, -1});
         }
+    };
 
-        cout << ans << endl;
+    auto sub_range = [&](ll l, ll r)
+    {
+        if (l <= r)
+        {
+            events.push_back({l, -1});
+            events.push_back({r + 1, 1});
+        }
+    };
+
+    // Projectile i is strictly in the air for T such that:
+    // H_i(T) > 0  => v(T-t) - 5(T-t)^2 > 0 => T-t < v/5
+    // For integer T, max(T-t) = floor((v-1)/5)
+    auto get_flight = [&](int i, ll &s, ll &e)
+    {
+        if (v[i] <= 5)
+        {
+            s = 1, e = 0;
+            return;
+        }
+        s = t[i] + 1;
+        e = t[i] + (v[i] - 1) / 5;
+    };
+
+    ll S1, E1;
+    get_flight(1, S1, E1);
+
+    L(i, 2, n)
+    {
+        ll Si, Ei;
+        get_flight(i, Si, Ei);
+        if (Si > Ei)
+            continue;
+
+        // Assume i wins for its entire flight
+        add_range(Si, Ei);
+
+        // If 1 is never in the air, i wins the whole range (already added)
+        if (S1 > E1)
+            continue;
+
+        // Find overlapping flight time [OL, OR]
+        ll OL = max(Si, S1);
+        ll OR = min(Ei, E1);
+        if (OL > OR)
+            continue;
+
+        // Inside the overlap, we must subtract where H_i(T) <= H_1(T)
+        // H_i - H_1 = AT + B
+        ll A = (v[i] + 10 * t[i]) - (v[1] + 10 * t[1]);
+        ll B = -(v[i] * t[i] + 5 * t[i] * t[i]) + (v[1] * t[1] + 5 * t[1] * t[1]);
+
+        // Subtract range where AT + B <= 0
+        if (A == 0)
+        {
+            if (B <= 0)
+                sub_range(OL, OR);
+        }
+        else if (A > 0)
+        {
+            // T <= -B/A
+            ll boundary = floor_div(-B, A);
+            sub_range(OL, min(OR, boundary));
+        }
+        else
+        { // A < 0
+            // T >= -B/A
+            ll boundary = ceil_div(-B, A);
+            sub_range(max(OL, boundary), OR);
+        }
     }
+
+    // Process events
+    sort(all(events));
+    vector<pair<ll, int>> compressed;
+    int current_sum = 0;
+    for (int i = 0; i < sz(events);)
+    {
+        ll cur_t = events[i].first;
+        while (i < sz(events) && events[i].first == cur_t)
+        {
+            current_sum += events[i].second;
+            i++;
+        }
+        compressed.push_back({cur_t, current_sum});
+    }
+
+    // Answer queries
+    while (q--)
+    {
+        ll x;
+        cin >> x;
+        auto it = upper_bound(all(compressed), make_pair(x, (int)2e9));
+        if (it == compressed.begin())
+        {
+            cout << 0 << "\n";
+        }
+        else
+        {
+            it--;
+            cout << it->second << "\n";
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    solve();
+    return 0;
 }
--- foo/gen.cpp
+++ foo/gen.cpp
@@ -1,46 +1,91 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// #define ll long long
+// #define nl "\n"
+// #define gp " "
+
+// mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+// inline ll rnd(ll l, ll r) {
+//     return uniform_int_distribution<ll>(l, r)(rng);
+
+// }
+
+// int main() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+
+//     // cout << 1 << endl;
+
+// //     int n = rnd(2, 5);
+// //     int k = (rnd(1, n));
+// //     int r = rnd(1, n);
+// //     int l = rnd(1, r);
+// //     cout << n << " " << k << " " << l << " " << r << "" << endl;
+
+// //     for(int i = 1; i <= n; ++i) {
+// //         int u, v;
+// //         u = rnd(1, 2*n);
+// //         // do{
+// //         //     v = rnd(1, n);
+// //         // } while(u == v || tree.count({u, v}) || tree.count({v, u}));
+// //         // tree.insert({u, v});
+// //         cout << u << gp;
+// //     }
+// // cout << nl;
+
+//     // int n = rnd(1, 10);
+//     // string a, b;
+//     // for(int i = 1; i <= n; i++) {
+//     //     a.push_back('a' + rnd(0, 2));
+//     //     b.push_back('a' + rnd(0, 2));
+//     // }
+//     // cout << n << endl;
+//     // cout << a << endl << b << endl;
+
+//     cout << 5 << " " << 8 << endl;
+//     for(int i = 0; i < 5; i++)  {
+//         cout << rnd(0, 6) << gp << rnd(1, 20) << endl;
+//     }
+//     for(int i = 0; i < 8; i++) {
+//         cout << i << endl;
+//     }
+
+
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
-#define ll long long
-#define nl "\n"
-#define gp " "
-
-mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
-inline ll rnd(ll l, ll r) {
-    return uniform_int_distribution<ll>(l, r)(rng);
-
-}
-
-int main() {
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    cout << 1 << endl;
+    int n = 8, q = 15;
+    cout << n << " " << q << "\n";
 
-//     int n = rnd(2, 5);
-//     int k = (rnd(1, n));
-//     int r = rnd(1, n);
-//     int l = rnd(1, r);
-//     cout << n << " " << k << " " << l << " " << r << "" << endl;
+    // Arya
+    ll s1 = 3, v1 = 10;   // v multiple of 5, short flight
+    cout << s1 << " " << v1 << "\n";
 
-//     for(int i = 1; i <= n; ++i) {
-//         int u, v;
-//         u = rnd(1, 2*n);
-//         // do{
-//         //     v = rnd(1, n);
-//         // } while(u == v || tree.count({u, v}) || tree.count({v, u}));
-//         // tree.insert({u, v});
-//         cout << u << gp;
-//     }
-// cout << nl;
+    vector<pair<ll,ll>> others = {
+        {3,10},   // identical
+        {3,11},   // same s, slightly faster
+        {3,9},    // same s, slightly slower
+        {4,10},   // same v, later
+        {2,10},   // same v, earlier
+        {3,5},    // v exactly 5 (edge of air)
+        {0,20},   // long flight, intersects
+    };
 
-    int n = rnd(1, 10);
-    string a, b;
-    for(int i = 1; i <= n; i++) {
-        a.push_back('a' + rnd(0, 2));
-        b.push_back('a' + rnd(0, 2));
+    for(auto &p: others){
+        cout << p.first << " " << p.second << "\n";
     }
-    cout << n << endl;
-    cout << a << endl << b << endl;
 
+    vector<ll> queries = {
+        0,1,2,3,4,5,6,7,8,9,10,11,12,13,14
+    };
+
+    for(ll T: queries) cout << T << "\n";
 }
